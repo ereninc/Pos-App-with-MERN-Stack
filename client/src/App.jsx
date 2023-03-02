61cd79754a50acbc141fc8bfee1da80353e193aa
Eren <erencan_inanci@hotmail.com>
2023-03-03 02:13:11 +0300

    router update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-import CategoryList from "./pages/home/components/categories";
 import Header from "./components/header";
-import ProductList from "./pages/home/components/products/product-list";
-import CartTotals from "./pages/home/components/cart/cart-totals";
+import HomePage from "./pages/home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CartPage from "./pages/cart";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-6">
-        <div className="categories-wrapper flex-1">
-          <CategoryList />
-        </div>
-        <div className="products-wrapper flex-[8]">
-          <ProductList />
-        </div>
-        <div className="cart-wrapper min-w-[250px] -mr-6 -mt-6 border-l md:pb-0 pb-24">
-          <CartTotals />
-        </div>
-      </div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import CartTotals from "./components/cart/cart-totals";
+import CategoryList from "./components/categories";
+import ProductList from "./components/products/product-list";
 
-export default function Home() {
-  return <div>Home</div>;
+export default function HomePage() {
+  return (
+    <div className="home px-6 flex md:flex-row flex-col justify-between gap-6">
+      <div className="categories-wrapper flex-1">
+        <CategoryList />
+      </div>
+      <div className="products-wrapper flex-[8]">
+        <ProductList />
+      </div>
+      <div className="cart-wrapper min-w-[250px] -mr-6 -mt-6 border-l md:pb-0 pb-24">
+        <CartTotals />
+      </div>
+    </div>
+  );
 }
