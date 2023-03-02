b4821f1991880f9abeced94c2e7de5d3a14ab6d7
Eren <erencan_inanci@hotmail.com>
2023-03-02 23:52:08 +0300

    cart update

--- client/src/containers/home-containers/cart-container/cart-total-container/index.jsx
+++ client/src/containers/home-containers/cart-container/cart-total-container/index.jsx
@@ -1,9 +1,11 @@
+import { Button } from "antd";
 import React from "react";
+import { ShoppingCartOutlined, ClearOutlined } from "@ant-design/icons";
 
 export default function CartTotalContainer() {
   return (
     <div className="cart h-full flex flex-col">
-      <h2 className="bg-orange-600 px-6 py-4 text-white text-xl text-center tracking-wide">
+      <h2 className="bg-blue-600 px-6 py-4 text-white text-xl text-center tracking-wide">
         Products in cart
       </h2>
       <div className="cart-items">
@@ -15,12 +17,32 @@ export default function CartTotalContainer() {
           <span>$90.00</span>
         </div>
         <div className="cart-total-item flex justify-between px-6 py-2 border-b">
-          <b>Tax</b>
-          <span>$9.00</span>
+          <b>Tax (%10)</b>
+          <span className="text-red-600">$9.00</span>
         </div>
-        <div className="cart-total-item flex justify-between px-6 py-2">
-          <b>Total</b>
+        <div className="cart-total-item flex justify-between px-6 py-2 border-b">
+          <b className="text-green-500">Total</b>
           <span>$99.00</span>
+        </div>
+        <div className="buttons flex flex-col justify-center gap-2 m-4">
+          <Button
+            type="primary"
+            size="large"
+            className=" text-white w-full flex items-center justify-center"
+            icon={<ShoppingCartOutlined />}
+          >
+            Checkout
+          </Button>
+
+          <Button
+            type="primary"
+            danger
+            size="large"
+            className=" text-white w-full flex items-center justify-center"
+            icon={<ClearOutlined />}
+          >
+            Clear Cart
+          </Button>
         </div>
       </div>
     </div>
