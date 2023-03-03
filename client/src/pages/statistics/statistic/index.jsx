1f252e741f023046265fb2614ba58ea5a9fd137f
Eren <erencan_inanci@hotmail.com>
2023-03-04 01:27:50 +0300

    statistics charts update

--- client/src/pages/statistics/statistic/index.jsx
+++ client/src/pages/statistics/statistic/index.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StatisticCard from "../components/card-components";
+import { Area } from "@ant-design/charts";
+import { Pie } from "@ant-design/plots";
 
 export default function StatisticsPage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
+  const asyncFetch = () => {
+    fetch(
+      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log("fetch data failed", error);
+      });
+  };
+  const config = {
+    data,
+    xField: "timePeriod",
+    yField: "value",
+    xAxis: {
+      range: [0, 1],
+    },
+  };
+
+  const data2 = [
+    {
+      type: "分类一",
+      value: 27,
+    },
+    {
+      type: "分类二",
+      value: 25,
+    },
+    {
+      type: "分类三",
+      value: 18,
+    },
+    {
+      type: "分类四",
+      value: 15,
+    },
+    {
+      type: "分类五",
+      value: 10,
+    },
+    {
+      type: "其他",
+      value: 5,
+    },
+  ];
+
+  const config2 = {
+    appendPadding: 10,
+    data: data2,
+    angleField: "value",
+    colorField: "type",
+    radius: 1,
+    innerRadius: 0.6,
+    label: {
+      type: "inner",
+      offset: "-50%",
+      content: "{value}",
+      style: {
+        textAlign: "center",
+        fontSize: 14,
+      },
+    },
+    interactions: [
+      {
+        type: "element-selected",
+      },
+      {
+        type: "element-active",
+      },
+    ],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: "pre-wrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        },
+        content: "AntV\nG2Plot",
+      },
+    },
+  };
+
   return (
     <div>
       <h1 className="font-bold text-center text-5xl">Statistics</h1>
@@ -40,6 +131,14 @@ export default function StatisticsPage() {
             }
             value={"800"}
           />
+        </div>
+      </div>
+      <div className="charts flex justify-between gap-2 p-6 lg:flex-row pb-24 flex-col items-center">
+        <div className="area-chart lg:w-1/2 lg:h-full h-72 flex-1">
+          <Area {...config} />
+        </div>
+        <div className="circle-chart lg:w-1/2 lg:h-full h-72 flex-1">
+          <Pie {...config2} />
         </div>
       </div>
     </div>
