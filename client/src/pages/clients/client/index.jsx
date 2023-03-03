8c0560ad56216ff4173b1e6aa22f3e774492e8a7
Eren <erencan_inanci@hotmail.com>
2023-03-04 00:13:37 +0300

    statistics card update

--- client/src/pages/clients/client/index.jsx
+++ client/src/pages/clients/client/index.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import DataTable from "../../../components/data-table";
 
 export default function ClientsPage() {
-  return <div>ClientsPage</div>;
+  const DATA = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const COLS = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="font-bold text-center text-5xl">Clients</h1>
+      <div className="p-6">
+        <DataTable dataSource={DATA} columns={COLS} />
+      </div>
+    </div>
+  );
 }
--- client/src/pages/statistics/statistic/index.jsx
+++ client/src/pages/statistics/statistic/index.jsx
@@ -1,5 +1,47 @@
 import React from "react";
+import StatisticCard from "../components/card-components";
 
 export default function StatisticsPage() {
-  return <div>StatisticsPage</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-center text-5xl">Statistics</h1>
+      <div className="p-6 main">
+        <div className="welcome">
+          <h2>
+            Welcome,{" "}
+            <span className="font-bold text-green-600 text-xl">admin</span>
+          </h2>
+        </div>
+        <div className="statistic-cards grid md:grid-cols-4 mt-8 sm:grid-cols-2 gap-6 w-full">
+          <StatisticCard
+            title={"Total Clients"}
+            img={
+              "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png"
+            }
+            value={"100"}
+          />
+
+          <StatisticCard
+            title={"Total Income"}
+            img={"https://cdn-icons-png.flaticon.com/512/4599/4599850.png"}
+            value={"$100"}
+          />
+
+          <StatisticCard
+            title={"Total Sells"}
+            img={"https://cdn-icons-png.flaticon.com/512/2361/2361914.png"}
+            value={"1200"}
+          />
+
+          <StatisticCard
+            title={"Total Products"}
+            img={
+              "https://cdn3d.iconscout.com/3d/premium/thumb/product-5806313-4863042.png"
+            }
+            value={"800"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
