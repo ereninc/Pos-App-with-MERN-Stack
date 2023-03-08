3e041ff8ac7bb5ed52a109e19d4538e1b45be9f6
Eren <erencan_inanci@hotmail.com>
2023-03-08 22:07:25 +0300

    secure password at database with bcryptjs

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,11 +1,17 @@
 const User = require("../models/User.js");
 const express = require("express");
 const router = express.Router(); //we can use HTTP methods like get, post, put, delete
+const bcrypt = require("bcryptjs");
 
 //register
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    // const newUser = new User(req.body);
+    // await newUser.save();
+    const { username, password, email } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ username, password: hashedPassword, email });
     await newUser.save();
     res.status(200).json("Regstered");
   } catch (error) {
