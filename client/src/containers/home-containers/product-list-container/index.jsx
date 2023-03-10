5f594aba08cb0e24986aa3f49752d7df5ce427ed
Eren <erencan_inanci@hotmail.com>
2023-03-10 18:17:31 +0300

    get product update

--- client/src/containers/home-containers/product-list-container/index.jsx
+++ client/src/containers/home-containers/product-list-container/index.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "../../../pages/home/components/products/product-item";
 
 export default function ProductListContainer() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch(
+          "http://localhost:5000/api/products/get-all-products"
+        );
+        const data = await res.json();
+        setProducts(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <div className="product-wrapper grid gap-4 grid-cols-card overflow-auto max-h-[calc(100vh-112px)] pb-6">
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
+      {products.map((pd) => {
+        return (
+          <ProductItem
+            key={pd._id}
+            productName={pd.title}
+            productPrice={pd.price}
+            productImage={pd.img}
+          />
+        );
+      })}
     </div>
   );
 }
--- client/src/pages/home/components/products/product-item/index.jsx
+++ client/src/pages/home/components/products/product-item/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-export default function ProductItem() {
+export default function ProductItem(props) {
   return (
     <div className="product-item border-2 hover:shadow-lg cursor-pointer transition-all duration-300 select-none">
       <div className="product-image">
         <img
-          src="https://hips.hearstapps.com/hmg-prod/images/apples-at-farmers-market-royalty-free-image-1627321463.jpg?resize=1200:*"
+          src={props.productImage}
           alt="product"
           className="h-28 object-cover w-full border-b-2"
         />
       </div>
       <div className="product-details flex flex-col items-center p-3">
-        <div className="product-name font-bold">Product Name</div>
-        <div className="product-price">Product Price</div>
+        <div className="product-name font-bold">{props.productName}</div>
+        <div className="product-price">${props.productPrice}</div>
       </div>
     </div>
   );
